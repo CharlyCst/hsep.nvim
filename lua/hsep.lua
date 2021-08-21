a3783f6305e23125123a5a25466f04a470f66e1b
CharlyCst <castes.ch@gmail.com>
2021-08-21 23:19:36 +0200

    Migrate to nvim 0.5 & add search feature
    - Update the plugin to use nvim 0.5 lua API
    - Refactor code to ease adding new features
    - Add separator search feature

--- lua/hsep.lua
+++ lua/hsep.lua
@@ -1,5 +1,5 @@
 local api = vim.api
-local buf, win, line, filetype
+local popup_buf, win, line, filetype
 
 -- —————————————————————————————— Configuration ————————————————————————————— --
 
@@ -21,54 +21,18 @@ local comments = {
     cpp    = '//',
 }
 
+-- The separator
+local sep = '—'
 
 -- ————————————————————————————————— Utils —————————————————————————————————— --
 
--- TODO: get the comments & width here
-
--- ——————————————————————————————— Separators ——————————————————————————————— --
-
-local function build_separator(text, comment)
-    local total_width = 80
-
-    -- check g:hsep_width
-    -- TODO: use vim.g once 0.5 hit stable
-    local override_width = api.nvim_get_var('hsep_width')
-    if type(override_width) == 'number' then
-        total_width = override_width
-    end
-
-    -- check g:hsep_language_width
-    -- TODO: use vim.g once 0.5 hit stable
-    local language_specific_width = api.nvim_get_var('hsep_language_width')
-    if type(language_specific_width) == 'table' then
-        if type(language_specific_width[filetype]) == 'number' then
-            total_width = language_specific_width[filetype]
-        end
-    end
-
-    -- build separator
-    local separator = ''
-    if string.len(text) == 0 then
-        separator = comment .. ' ' .. string.rep('—', total_width - 2 - 2 * string.len(comment)) .. ' ' .. comment
-    else
-        local separator_width = total_width - string.len(text) - 2 * string.len(comment) - 4
-        local left_separator = string.rep('—', math.floor(separator_width/2))
-        local right_separator = string.rep('—', math.ceil(separator_width/2))
-        separator = comment .. ' ' .. left_separator .. ' ' .. text .. ' ' .. right_separator .. ' ' .. comment
-    end
-    return separator
-end
-
-local function insert_separator()
-    local text = api.nvim_buf_get_lines(buf, 0, 1, false)[1]
-
-    -- get comment from filetype, or user provided table
+-- Get comment from filetype, or user provided table.
+local function get_comment_string()
     local comment = comments[filetype]
     if not comment then
         comment = '//'
     end
-    -- local override_comments = api.nvim_get_var('hsep_language_comments')
+
     local override_comments = vim.g.hsep_language_comments
     if type(override_comments) == 'table' then
         if type(override_comments[filetype]) == 'string' then
@@ -76,13 +40,61 @@ local function insert_separator()
         end
     end
 
-    local separator = build_separator(text, comment)
+    return comment
+end
+
+-- Get the separator width.
+local function get_separator_width()
+    local width = 80
+
+    -- check g:hsep_width
+    local override_width = vim.g.hsep_width
+    if type(override_width) == 'number' then
+        width = override_width
+    end
+
+    -- check g:hsep_language_width
+    local language_specific_width = vim.g.hsep_language_width
+    if type(language_specific_width) == 'table' then
+        if type(language_specific_width[filetype]) == 'number' then
+            width = language_specific_width[filetype]
+        end
+    end
+
+    return width
+end
+
+-- ——————————————————————————————— Separators ——————————————————————————————— --
+
+-- Return the separator for a given text.
+local function build_separator(text)
+    local comment = get_comment_string()
+    local total_width = get_separator_width()
+    local separator = ''
+
+    if string.len(text) == 0 then
+        separator = comment .. ' ' .. string.rep(sep, total_width - 2 - 2 * string.len(comment)) .. ' ' .. comment
+    else
+        local separator_width = total_width - string.len(text) - 2 * string.len(comment) - 4
+        local left_separator = string.rep(sep, math.floor(separator_width/2))
+        local right_separator = string.rep(sep, math.ceil(separator_width/2))
+        separator = comment .. ' ' .. left_separator .. ' ' .. text .. ' ' .. right_separator .. ' ' .. comment
+    end
+    return separator
+end
+
+-- Insert a separator with the popup's text under the cursor location.
+local function insert_separator()
+    local text = api.nvim_buf_get_lines(popup_buf, 0, 1, false)[1]
+
+    local separator = build_separator(text)
     require'hsep'._close_window()
     api.nvim_buf_set_lines(0, line, line, false, {separator})
 end
 
 -- ——————————————————————————— Windows & Buffers ———————————————————————————— --
 
+-- Set the Hsep mappings inside the given buffer (e.g. the popup window)
 local function set_mappings(buf)
     local mappings = {
         ['<Cr>'] = '_insert_separator()',
@@ -98,7 +110,8 @@ end
 
 local function open_window()
     -- create the buffer
-    buf = api.nvim_create_buf(false, true)
+    local buf = api.nvim_create_buf(false, true)
+    popup_buf = buf
     api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
     set_mappings(buf)
 
@@ -148,22 +161,34 @@ local function open_window()
     -- enter insert mode and exit on enter
     api.nvim_command('startinsert')
     -- todo: how to close windows on focus loss?
-
 end
 
 local function close_window()
     api.nvim_win_close(win, true)
-    buf = nil
+    popup_buf = nil
+end
+
+-- ———————————————————————— Interact with Separators ———————————————————————— --
+
+local function search_separators()
+    local comment = get_comment_string()
+    vim.cmd('silent! /' .. comment .. '\\s*' .. string.rep(sep, 5) .. '.*' .. string.rep(sep, 5) .. '\\s*' .. comment)
 end
 
 -- —————————————————————————————————— Main —————————————————————————————————— --
 
 -- The main HSep function
-local function hsep()
-    local position = api.nvim_win_get_cursor(0)
-    line = position[1]
+local function hsep(args)
+    -- Update global variables
+    line = api.nvim_win_get_cursor(0)[1]
     filetype = api.nvim_buf_get_option(0, 'filetype')
-    open_window()
+
+    if args == 'search' then
+        search_separators()
+    else
+        -- Default to inserting a separator
+        open_window()
+    end
 end
 
 return {
--- plugin/hsep.vim
+++ plugin/hsep.vim
@@ -3,7 +3,7 @@ if exists('g:loaded_hsep') | finish | endif
 let s:save_cpo = &cpo " save user cpoptions
 set cpo&vim           " reset to default
 
-command! HSep lua require'hsep'.hsep()
+command! -nargs=? HSep call luaeval("require'hsep'.hsep(_A)", <q-args>)
 
 let &cpo = s:save_cpo " restore cpoptions
 unlet s:save_cpo
